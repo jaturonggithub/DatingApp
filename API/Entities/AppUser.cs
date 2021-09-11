cbccb6da1e2ed97e64340af0256daae357435af0
jaturonggithub <jaturong@persona-solution.com>
2021-09-11 15:40:26 +0700

    End of section 14

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -27,9 +27,7 @@ namespace API.Entities
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; }
 
-        /*public int getAge()
-        {
-            return DateOfBirth.CalculateAge();
-        }*/
+        public ICollection<UserLike> LikedByUsers { get; set; }
+        public ICollection<UserLike> LikedUsers { get; set; }
     }
 }
