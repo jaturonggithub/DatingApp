7e1c258bcc80c404d0e1b1225c42d6a106647f83
jaturonggithub <jaturong@persona-solution.com>
2021-09-18 07:56:25 +0700

    Section 19-pre switch to postgres

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -12,7 +12,6 @@ import { PresenceService } from './presence.service';
   providedIn: 'root'
 })
 export class AccountService {
-  //baseUrl = 'https://localhost:5001/api/';
   baseUrl = environment.apiUrl;
 
   private currentUserSource = new ReplaySubject<User>(1);
